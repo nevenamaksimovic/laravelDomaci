48c8415939509ae2627103286c87d88a2d583245
nevenamaksimovic <nevena.maksimovic@gmail.com>
2022-11-08 00:03:40 +0100

    Zavrseni modeli

--- app/Models/Credit.php
+++ app/Models/Credit.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Credit extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'min_amount',
+        'max_amount',
+        'rate',
+        'min_period',
+        'max_period'
+    ];
 }
--- app/Models/CreditRequest.php
+++ app/Models/CreditRequest.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class CreditRequest extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'client_id',
+        'credit_id',
+        'amount',
+        'period'
+    ];
+
+    protected $with = ['client', 'credit'];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function credit()
+    {
+        return $this->belongsTo(Credit::class);
+    }
 }
