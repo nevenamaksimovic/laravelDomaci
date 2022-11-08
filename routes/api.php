6e3b96474233df9478a866a08d6a87461cec08f5
nevenamaksimovic <nevena.maksimovic@gmail.com>
2022-11-08 21:12:56 +0100

    Dodate putanje za login i registraciju

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Http\Controllers\api\KreditController;
+use App\Http\Controllers\api\ZahtevController;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return response()->json(["user" => $user, "token" => $user->createToken($request->email)->plainTextToken]);
+});
+
+Route::post('/register', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if ($user) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $user = User::create([
+        "name" => $request->name,
+        "email" => $request->email,
+        "email" => Hash::make($request->password),
+    ]);
+
+    return response()->json(["user" => $user, "token" => $user->createToken($request->email)->plainTextToken]);
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
 });
